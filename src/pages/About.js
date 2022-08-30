af7a341a508f57a9e460f4fc47a6502683b83b44
lancey1 <lanceyu619@gmail.com>
2022-08-29 22:49:16 -0400

    minor styling changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,24 +9,40 @@ export default function About() {
       <div className={`${styles.divContainer}`}>
         <img className={`${styles.house}`} src={house} alt="Daycare Area" />
         <p className={`${styles.divp1}`}>
-          Little Wonders is a home daycare located in Bradford ON. The daycare
-          is air conditioned and heated. It is also pet and smoke free. Operated
-          by Ms. Rohit, an experienced Registered Early Childhood Educator with
-          more than 8 years of experience. Open for ages 30 months and above.
+          <b>About Little Wonders</b>
+          {"\n"}
+          {"\n"}
+          Little Wonders is a home daycare located in Bradford Ontario. Major
+          intersection of Barrie St. and Holland St West.
+          {"\n"}
+          {"\n"}
+          The daycare is air-conditioned and heated. It is also pet and smoke
+          free. There is ample outdoor space for physical activity.
+          {"\n"}
+          {"\n"}
+          Operated by Ms. Rohit, an experienced Registered Early Childhood
+          Educator with more than 8 years of experience.{"\n"}
+          {"\n"} We are currently accepting children ages 30 months and above.
         </p>
         <img className={`${styles.pfp}`} src={pfp} alt="Ms. Rohit" />
         <p className={`${styles.divp2}`}>
-          Ms. Rohit is a mother of two daughters aged 12 and 15 years old. She
-          is proficient in multiple languages: English, Punjabi and Hindi. She
-          also understands a few basic Russian words. She graduated with Honors
-          from Humber College in Early Childhood Education, and awarded a
-          Master's Degree in Political Science and Punjabi in India. Ms. Rohit
-          previously worked in child care centers that are well-renowned in the
-          local communities of Bradford, Thornhill, and Vaughan since 2013. She
-          is fully vaccinated including Covid-19 vaccines, first-aid CPR
-          certified and vulnerable sector clearance. Her love and
-          expertise in children fuelled her passion to opening her very own
-          daycare center.
+          Ms. Rohit is a mother of two daughters aged 12 and 15 years old.{" "}
+          {"\n"}
+          {"\n"} She is proficient in multiple languages: English, Punjabi and
+          Hindi. She also understands a few basic Russian words. {"\n"}
+          {"\n"} She graduated with Honors from Humber College in Early
+          Childhood Education, and was awarded a Master's Degree in Political
+          Science and Punjabi in India. {"\n"}
+          {"\n"} Ms. Rohit previously worked in child care centers that are
+          well-renowned in the local communities of Bradford, Thornhill, and
+          Vaughan since 2013.
+          {"\n"}
+          {"\n"}
+          She is first-aid CPR certified, fully vaccinated including Covid-19,
+          and has vulnerable sector clearance.
+          {"\n"}
+          {"\n"} Her passion and expertise in children motivated her into
+          opening her very own daycare center.
         </p>
       </div>
     </div>
--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -7,19 +7,20 @@ export default function footer() {
         <h3>About Us</h3>
         <p>
           Little Wonders is a daycare located in Bradford ON. Operated by an
-          experienced Registered Early Childhood Educator with more than
-          8 years of experience. Open for ages 30 months and above.
+          experienced Registered Early Childhood Educator with more than 8 years
+          of experience.
         </p>
+        <p>Open for ages 30 months and above.</p>
       </div>
       <div className={`${styles.contact}`}>
         <h3>Contact Us</h3>
         <p>84 Porter St. Bradford, ON (Barrie St. and Holland St West)</p>
         <p>Phone: 647-524-4429</p>
         <p>Email: rohitajaysharma84@gmail.com</p>
-        <p>Hours: Monday to Friday  7:30 AM to 5:30 PM</p>
+        <p>Hours: Monday to Friday 7:30 AM to 5:30 PM</p>
       </div>
       <div className={`${styles.links}`}>
-        <h3>Links</h3>
+        <h3>Quick Links</h3>
         <p>About</p>
         <p></p>
         <p>Contact</p>
